1467cf9e91e4b037ee07fa100ac0712df3c0129f
AlexandrIgn <asignatyev@mail.ru>
2019-04-05 21:16:48 +0400

    complete 7 step

---
+++ src/games/BrainPrime.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BrainGames\BrainPrime;
+
+use function \cli\line;
+use function \cli\prompt;
+use function BrainGames\Engine\run;
+
+const ANNOTATION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function isPrime(int $num)
+{
+    if ($num < 2) {
+        return false;
+    }
+
+    for ($i = 2; $i <= $num / 2; $i++) {
+        if ($num % $i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+function runBrainPrime()
+{
+    $getQuestionAndAnswer = function () {
+        $question = rand(1, 99);
+        $answer = isPrime($question) ? 'yes' : 'no';
+        return [$question, $answer];
+    };
+    run($getQuestionAndAnswer, ANNOTATION);
+}
